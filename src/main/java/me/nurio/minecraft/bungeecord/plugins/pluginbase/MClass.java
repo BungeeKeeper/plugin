e4eb58927f2a956631491e44dace3fcb61c61461
Lucas Goyeche <29938718+0gg@users.noreply.github.com>
2020-05-25 20:22:02 -0300

    Updated main class

--- src/main/java/me/nurio/minecraft/bungeecord/plugins/pluginbase/MClass.java
+++ src/main/java/me/nurio/minecraft/bungeecord/plugins/pluginbase/MClass.java
@@ -1,12 +1,26 @@
 package me.nurio.minecraft.bungeecord.plugins.pluginbase;
 
+import me.nurio.minecraft.bungeecord.plugins.pluginbase.connection.sockets.ConnectionManager;
+import me.nurio.minecraft.bungeecord.plugins.pluginbase.listeners.PlayerConnectionListener;
+import me.nurio.minecraft.bungeecord.plugins.pluginbase.listeners.PingListener;
+import me.nurio.minecraft.bungeecord.plugins.pluginbase.listeners.ServerListener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class MClass extends Plugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Guru guru!");
+        getLogger().info("Anguilas Activadas");
+        getProxy().getPluginManager().registerListener(this, new PlayerConnectionListener(this));
+        getProxy().getPluginManager().registerListener(this, new PingListener(this));
+        getProxy().getPluginManager().registerListener(this, new ServerListener(this));
+
+        ConnectionManager.enable();
+    }
+
+    @Override
+    public void onDisable() {
+        ConnectionManager.disable();
     }
 
 }
