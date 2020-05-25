7c9c0d73c2f8b9649d976574c8030674cb48cf78
Lucas Goyeche <29938718+0gg@users.noreply.github.com>
2020-05-25 20:16:17 -0300

    Added Identity utils

---
+++ src/main/java/me/nurio/minecraft/bungeecord/plugins/pluginbase/utils/IdentityUtil.java
@@ -0,0 +1,16 @@
+package me.nurio.minecraft.bungeecord.plugins.pluginbase.utils;
+
+import java.util.Random;
+
+public class IdentityUtil {
+
+    private static Random random = new Random();
+
+    public static long timeBasedId() {
+        long time = System.currentTimeMillis() * 10_000;
+        int randomInt = random.nextInt(9000) + 1000;
+
+        return time + randomInt;
+    }
+
+}
